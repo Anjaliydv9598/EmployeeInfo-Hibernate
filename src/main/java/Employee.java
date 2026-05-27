8fc74b32111caf11d978c4c56b644da43a3946e2
Anjaliydv9598 <anjaliydv9598.com>
2026-05-28 03:57:16 +0530

    Added the section to fetch it using JPQL

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -3,26 +3,36 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "employee")
+@Table(name = "employeedetail")
 public class Employee {
 	
 	@Id
 	private int eid;
 	private String ename;
     private String email;
+    private int age;                  
+    private double salary; 
     private long ephone;
     private String address;
+    private boolean isActive;          // true/false
+    private char gender;  
     
     public Employee() {
     }
-    
-    Employee(int eid , String ename ,String email , long ephone , String address ){
-    	this.eid=eid;
-    	this.ename=ename;
-    	this.email=email;
-    	this.ephone=ephone;
-    	this.address=address;
-    }
+
+	public Employee(int eid, String ename, String email, int age, double salary, long ephone, String address,
+			boolean isActive, char gender) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.email = email;
+		this.age = age;
+		this.salary = salary;
+		this.ephone = ephone;
+		this.address = address;
+		this.isActive = isActive;
+		this.gender = gender;
+	}
 
 	public int getEid() {
 		return eid;
@@ -48,6 +58,22 @@ public class Employee {
 		this.email = email;
 	}
 
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
 	public long getEphone() {
 		return ephone;
 	}
@@ -63,6 +89,26 @@ public class Employee {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+
+
+
 
 	
    
--- src/main/java/SaveEmployee.java
+++ src/main/java/SaveEmployee.java
@@ -1,7 +1,10 @@
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import org.hibernate.cfg.Configuration;
 
@@ -13,17 +16,38 @@ public class SaveEmployee {
 			EntityManager em=emf.createEntityManager();
 			EntityTransaction et=em.getTransaction();
 			
-	    	Employee e = new Employee(2,"Anjali" , "asd@gmail.com" , 1234567890 , "gurgaon" );
+//	    	Employee e = new Employee(2,"Alia" , "aad@gmail.com" ,21,300000, 1233456890 , "Chandigarh",false , 'F' );
 	    	
-	    	et.begin();
+	    	
+//	    	et.begin();
 		 
-	    	em.persist(e);
-	    	et.commit();
-	    	System.out.println("Data Saved Successfully");
-
-	        // Close Resources
-	        em.close();
-	        emf.close();
+//	    	em.persist(e);
+//	    	em.merge(e);
+//	    	et.commit();
+//	    	System.out.println("Data Saved Successfully");
+//
+	Query query = em.createQuery("select e from Employee e");
+			
+	    	List <Employee> li = query.getResultList();
+	    	for(Employee e2:li) {
+	    		System.out.println("Employee Id : "+e2.getEid()+"\n"+"Employee Name: "+e2.getEname()+"\n"+"Employee Address : "+e2.getAddress()+"\n"+"Employee Age: " +e2.getAge()+"\n"+"Employee Email: "+e2.getEmail()+"\n"+"Employee Phone Number: "+e2.getEphone()+"\n"+"Employee's Gender :"+e2.getGender()+"\n"+"Employee Salary : "+e2.getSalary());
+	    	
+	    
+	    		
+//	        // Close Resources
+//	        em.close();
+//	        emf.close();
+	    	
+//			Employee e = new Employee(3,"Deepika" , "d@gmail.com" ,22,400000, 12334890 , "Pune",true , 'F' );
+	    	
+	    	
+	    	
+	   
+	 		
+	    		
+	    		
+	    		
+	    	}
 	    }
 
 }
